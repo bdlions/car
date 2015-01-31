6388260b96a8e90cd35a79324c79a9f1adf1ba3d
Redoy <nazmulislamredoy@gmail.com>
2015-01-31 18:12:19 +0600

    new features updated

--- resources/js/slider_feedback.js
+++ resources/js/slider_feedback.js
@@ -1,14 +1,18 @@
+$(function() {
+    $('.pull_feedback').on("click", function(e) {
+        var left = -390;
+        var screenWidth = $(window).width();
+        if (screenWidth <= 991) {
+            left = -220;
+        }
+        console.log(left);
+        $("#feedback").animate({left: $("#feedback").css("left") == "0px" ? left + "px" : "0px"});
+    });
 
-            $(function(){
-                $('.pull_feedback').on("click", function(e){
-                    var left = -390;
-                    var screenWidth = $( window ).width();
-                    if(screenWidth <= 991){
-                        left = -220;
-                    }
-                    console.log(left);
-                    $("#feedback").animate({left:$("#feedback").css("left") == "0px" ? left +"px" :"0px"});
-                });
-                
-            });
+    $('#chat_opener').on("click", function(e) {
+        var bottom = -390;
+        $("#chat_box").animate({bottom: $("#chat_box").css("bottom") == "0px" ? bottom + "px" : "0px"});
+    });
+
+});
     
